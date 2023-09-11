9dd57fffbb07f10a77b2e3ef8dde2a21baa1ed70
noixido <112402221+noixido@users.noreply.github.com>
2023-09-12 02:08:02 +0700

    commit ke 25, perbaiki base url di tabel-pendaftaran.blade.php biar kalo base url diganti, gk mesti ganti satu satu di tabel-pendaftaran, dan install + update depedency

--- resources/views/akademik/dataPendaftaran/tabel-pendaftaran.blade.php
+++ resources/views/akademik/dataPendaftaran/tabel-pendaftaran.blade.php
@@ -50,61 +50,49 @@
             <td>{{ $row->mahasiswa->email }}</td>
             <td>{{ $row->tugas_akhir->dosen->nama_dosen }}</td>
             <td>{{ $row->tugas_akhir->judul_tugas_akhir }}</td>
-            <td>
-                <a href="http://127.0.0.1:8000/bimbingan/mahasiswa/{{ $row->mahasiswa->nim }}">{{
-                    'http://127.0.0.1:8000/bimbingan/mahasiswa/'.$row->mahasiswa->nim }}</a>
+            <td><a href="{{ url('/') }}/bimbingan/mahasiswa/{{ $row->mahasiswa->nim }}">{{url('/').'/bimbingan/mahasiswa/'.$row->mahasiswa->nim
+                    }}</a>
             </td>
             <td>{{ $row->ipk_saat_ini }}</td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->pas_foto }}">{{ 'http://127.0.0.1:8000/storage/'.
-                    $row->pas_foto }}</a></td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->scan_bukti_spp }}">{{ 'http://127.0.0.1:8000/storage/'.
-                    $row->scan_bukti_spp }}</a></td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->scan_ijazah_terakhir }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_ijazah_terakhir }}</a></td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->scan_akta_kelahiran }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_akta_kelahiran }}</a></td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->scan_kartu_keluarga }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_kartu_keluarga }}</a></td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->scan_sertifikat_peka }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_sertifikat_peka }}</a></td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->scan_sertifikat_toefl }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_sertifikat_toefl }}</a></td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->scan_sertifikat_ujikom_1 }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_sertifikat_ujikom_1 }}</a></td>
-            <td><a href="http://127.0.0.1:8000/storage/{{ $row->scan_sertifikat_ujikom_2 }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_sertifikat_ujikom_2 }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->pas_foto }}">{{ url('/').'/storage/'.$row->pas_foto }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->scan_bukti_spp }}">{{ url('/').'/storage/'.$row->scan_bukti_spp
+                    }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->scan_ijazah_terakhir }}">{{url('/').'/storage/'.$row->scan_ijazah_terakhir
+                    }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->scan_akta_kelahiran }}">{{url('/').'/storage/'.$row->scan_akta_kelahiran
+                    }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->scan_kartu_keluarga }}">{{url('/').'/storage/'.$row->scan_kartu_keluarga
+                    }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->scan_sertifikat_peka }}">{{url('/').'/storage/'.$row->scan_sertifikat_peka
+                    }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->scan_sertifikat_toefl }}">{{url('/').'/storage/'.$row->scan_sertifikat_toefl
+                    }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->scan_sertifikat_ujikom_1 }}">{{url('/').'/storage/'.$row->scan_sertifikat_ujikom_1
+                    }}</a></td>
+            <td><a href="{{ url('/') }}/storage/{{ $row->scan_sertifikat_ujikom_2 }}">{{url('/').'/storage/'.$row->scan_sertifikat_ujikom_2
+                    }}</a></td>
             <td>
                 @if ($row->scan_sertifikat_ujikom_3 === null)
                 -
                 @else
-                <a href="http://127.0.0.1:8000/storage/{{ $row->scan_sertifikat_ujikom_3 }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_sertifikat_ujikom_3 }}</a>
+                <a href="{{ url('/') }}/storage/{{ $row->scan_sertifikat_ujikom_3 }}">{{url('/').'/storage/'.$row->scan_sertifikat_ujikom_3
+                    }}</a>
                 @endif
             </td>
             <td>
                 @if ($row->scan_sertifikat_ujikom_4 === null)
                 -
                 @else
-                <a href="http://127.0.0.1:8000/storage/{{ $row->scan_sertifikat_ujikom_4 }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_sertifikat_ujikom_4 }}</a>
+                <a href="{{ url('/') }}/storage/{{ $row->scan_sertifikat_ujikom_4 }}">{{url('/').'/storage/'.$row->scan_sertifikat_ujikom_4
+                    }}</a>
                 @endif
             </td>
             <td>
                 @if ($row->scan_sertifikat_ujikom_5 === null)
                 -
                 @else
-                <a href="http://127.0.0.1:8000/storage/{{ $row->scan_sertifikat_ujikom_5 }}">{{
-                    'http://127.0.0.1:8000/storage/'.
-                    $row->scan_sertifikat_ujikom_5 }}</a>
+                <a href="{{ url('/') }}/storage/{{ $row->scan_sertifikat_ujikom_5 }}">{{url('/').'/storage/'.$row->scan_sertifikat_ujikom_5
+                    }}</a>
                 @endif
             </td>
         </tr>
